8981a80f80b2900de047a5aebb937d2b2e104dce
verpivia-1 <cyrr0070088@gmail.com>
2025-12-03 10:21:58 +0900

    <feat>ref

---
+++ bosh/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace bosh
+{
+  
+    internal class Program
+    {
+        static void Swap(ref int a,ref int b)
+        {
+            int c = a;
+            a = b;
+            b = c;
+            
+        }
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 20;
+            Swap(ref x,ref y);
+            Console.WriteLine($"{x},{y}");
+
+        }
+    }
+}
